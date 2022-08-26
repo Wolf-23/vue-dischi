916f4cf41c7031bdd7be86d3278207d51b209cdf
Riccardo <lupinu.ric@gmail.com>
2022-08-26 16:27:51 +0200

    Project: Start

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <img src="../assets/logo-small.png" alt="Logo Spotify">
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'MyHeader'
+}
+</script>
+
+<style lang="scss">
+  @import '../assets/styles/general.scss';
+   header {
+    width: 100%;
+    height: 80px;
+    background-color: #2E3A46;
+    img {
+        height: 100%;
+    }
+   }
+</style>
---
+++ src/components/MyMain.vue
@@ -0,0 +1,28 @@
+<template>
+   <main>
+
+   </main>
+</template>
+
+<script>
+export default {
+    name: 'MyMain',
+
+    data() {
+        return {
+            diskList: []
+        }
+    }
+}
+
+
+</script>
+
+<style lang="scss">
+  @import '../assets/styles/general.scss';
+   main {
+    width: 100%;
+    height: calc(100vh - 80px);
+    background-color:#1E2D3B;
+   }
+</style>
